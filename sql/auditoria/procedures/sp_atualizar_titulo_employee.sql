29b1ca2d83ac2442c58a7d8dd819b0674763126b
IagoTannusb <iagotannusb.pomponet@gmail.com>
2025-11-19 06:47:12 -0300

    mudança na estrutura do projeto

---
+++ sql/auditoria/procedures/sp_atualizar_titulo_employee.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE atualizar_titulo_employee(
+    p_employee_id INT,
+    p_new_title VARCHAR(100)
+)
+AS $$
+BEGIN
+    UPDATE employees
+    SET title = p_new_title
+    WHERE employee_id = p_employee_id;
+END;
+$$ LANGUAGE plpgsql;
